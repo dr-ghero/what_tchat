6eac31bc207428c09023d7264893f25a9153d4f2
dr-ghero <akoaetmbatkam@gmail.com>
2023-10-11 12:52:06 +0200

    source

---
+++ src/main/java/com/example/tchat/beans/Utilisateurs.java
@@ -0,0 +1,4 @@
+package com.example.tchat.beans;
+
+public class Utilisateurs {
+}
---
+++ src/main/java/com/example/tchat/servlets/IndexServlet.java
@@ -0,0 +1,19 @@
+package com.example.tchat.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet", value = "/IndexServlet")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
